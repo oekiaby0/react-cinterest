286e0b3380001dce909816112d03c056900a9662
oekiaby0 <oekiaby0@gmail.com>
2023-10-13 04:40:38 +0100

    Added table view for unit period overview

--- src/App.tsx
+++ src/App.tsx
@@ -10,74 +10,79 @@ import {
 } from '@mui/material';
 
 enum PERIOD {
-    ANNUALLY,
-    QUARTERLY,
-    MONTHLY,
+    DAILY,
     WEEKLY,
-    DAILY
+    MONTHLY,
+    QUARTERLY,
+    ANNUALLY
 }
 
-function periodToUnit(p: PERIOD) {
+function periodToUnit(p: PERIOD): string {
     let unit
     switch (p) {
-        case PERIOD.ANNUALLY: unit = "Year"; break;
-        case PERIOD.QUARTERLY: unit = "Quarter"; break;
-        case PERIOD.MONTHLY: unit = "Month"; break;
-        case PERIOD.WEEKLY: unit = "Week"; break;
-        case PERIOD.DAILY: unit = "Day"; break;
-        default: unit="Unknown unit"
+        case PERIOD.ANNUALLY:
+            unit = "Year";
+            break;
+        case PERIOD.QUARTERLY:
+            unit = "Quarter";
+            break;
+        case PERIOD.MONTHLY:
+            unit = "Month";
+            break;
+        case PERIOD.WEEKLY:
+            unit = "Week";
+            break;
+        case PERIOD.DAILY:
+            unit = "Day";
+            break;
+        default:
+            unit = "Unknown unit"
     }
     return unit
 }
 
-function periodToDays(p: PERIOD) {
+function periodToDays(p: PERIOD): number {
     let days
     switch (p) {
-        case PERIOD.ANNUALLY: days = 365; break;
-        case PERIOD.QUARTERLY: days = 365/4; break;
-        case PERIOD.MONTHLY: days = 365/12; break;
-        case PERIOD.WEEKLY: days = 7; break;
-        case PERIOD.DAILY: days = 1; break;
+        case PERIOD.ANNUALLY:
+            days = 365;
+            break;
+        case PERIOD.QUARTERLY:
+            days = 365 / 4;
+            break;
+        case PERIOD.MONTHLY:
+            days = 365 / 12;
+            break;
+        case PERIOD.WEEKLY:
+            days = 7;
+            break;
+        case PERIOD.DAILY:
+            days = 1;
+            break;
     }
     return days
+}
+
+function smallestPeriod(a: PERIOD, b: PERIOD) {
+    return a < b ? a : b
 }
 
 function calculateResults(
     principal: number,
     interestRate: number, interestRatePeriod: PERIOD,
     contribution: number, contributionPeriod: PERIOD,
-    years: number, compoundPeriod: PERIOD
+    years: number
 ): [[number], [number]] {
     let interestRateDays = periodToDays(interestRatePeriod)
-    let compoundPeriodDays = periodToDays(compoundPeriod)
     let contributionDays = periodToDays(contributionPeriod)
-    let smallestPeriodDays = Math.min(compoundPeriodDays, contributionDays)
-    let convertedRate = Math.pow(1 + interestRate/100, smallestPeriodDays/interestRateDays)
-    let compoundSmaller = compoundPeriodDays < contributionDays
-    let lastC = 0
-    let results: [[number], [number]] = [[0], [0]]
-    for (let n= 1; n < years * 365/smallestPeriodDays + 1; n++) {
-        let balance = results[0][n-1]
-        if (compoundPeriod === contributionPeriod) {
-            balance += contribution
-            balance *= convertedRate
-        } else if (compoundSmaller) {
-            balance += contribution
-            results[1].push(contribution)
-            if (n * smallestPeriodDays > lastC + compoundPeriodDays) {
-                lastC = n * smallestPeriodDays
-                balance *= convertedRate
-            }
-        } else {
-            if (n * smallestPeriodDays > lastC + contributionDays) {
-                lastC = n * smallestPeriodDays
-                balance += contribution
-                results[1].push(contribution)
-            } else {
-                results[1].push(0)
-            }
-            balance *= convertedRate
-        }
+    let convertedRate = Math.pow(1 + interestRate / 100, (contributionDays / interestRateDays))
+    console.log(convertedRate)
+    let results: [[number], [number]] = [[principal], [principal]]
+    for (let n = 1; n < years * 365 / contributionDays + 1; n++) {
+        let balance = results[0][n - 1]
+        balance *= convertedRate
+        balance += contribution
+        results[1].push(contribution)
         results[0].push(balance)
     }
     return results
@@ -90,8 +95,25 @@ function convertToTable(results: [[number], [number]]): [{
     deposits: number,
     totalDeposits: number
 }] {
+    let table: [{
+        balance: number,
+        interest: number,
+        cuInterest: number,
+        deposits: number,
+        totalDeposits: number
+    }] = [{balance: results[0][0], interest: 0, cuInterest: 0, deposits: results[1][0], totalDeposits: results[1][0]}]
 
-    return [{balance: 0, interest: 0, cuInterest: 0, deposits: 0, totalDeposits: 0}]
+    for (let i = 1; i < results[0].length; i++) {
+        let row = {balance: 0, interest: 0, cuInterest: 0, deposits: 0, totalDeposits: 0}
+        row.balance = results[0][i]
+        row.deposits = results[1][i]
+        row.totalDeposits = row.deposits + table[i - 1].totalDeposits
+        row.interest = row.balance - (table[i - 1].balance + row.deposits)
+        row.cuInterest = row.interest + table[i - 1].cuInterest;
+        table.push(row)
+    }
+
+    return table
 }
 
 function ParametersSection({
@@ -101,7 +123,6 @@ function ParametersSection({
                                contribution, setContribution,
                                contributionPeriod, setContributionPeriod,
                                yearsCount, setYearsCount,
-                               compoundPeriod, setCompoundPeriod,
                                handleCalculate
                            }: {
                                principal: string, setPrincipal: (p: string) => void,
@@ -110,7 +131,6 @@ function ParametersSection({
                                contribution: string, setContribution: (contribution: string) => void,
                                contributionPeriod: PERIOD, setContributionPeriod: (period: PERIOD) => void,
                                yearsCount: string, setYearsCount: (years: string) => void,
-                               compoundPeriod: PERIOD, setCompoundPeriod: (period: PERIOD) => void
                                handleCalculate: (e: any) => void
                            }
 ) {
@@ -188,30 +208,15 @@ function ParametersSection({
                     </Select>
                 </FormControl>
             </div>
-            <div>
-                <TextField
-                    label="Number of Years"
-                    id="years-input"
-                    sx={{width: '15ch'}}
-                    variant="filled"
-                    size="small"
-                    value={yearsCount}
-                    onChange={e => setYearsCount(e.target.value)}
-                />
-                <FormControl sx={{m: 1, width: '12ch'}}>
-                    <Select
-                        value={compoundPeriod}
-                        onChange={e => setCompoundPeriod(e.target.value as PERIOD)}
-                        size="small"
-                    >
-                        <MenuItem value={PERIOD.ANNUALLY}>Annually</MenuItem>
-                        <MenuItem value={PERIOD.QUARTERLY}>Quarterly</MenuItem>
-                        <MenuItem value={PERIOD.MONTHLY}>Monthly</MenuItem>
-                        <MenuItem value={PERIOD.WEEKLY}>Weekly</MenuItem>
-                        <MenuItem value={PERIOD.DAILY}>Daily</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
+            <TextField
+                label="Number of Years"
+                id="years-input"
+                sx={{m: 1, width: '15ch'}}
+                variant="filled"
+                size="small"
+                value={yearsCount}
+                onChange={e => setYearsCount(e.target.value)}
+            />
             <Button variant="contained" size="medium"
                     onClick={handleCalculate}
             >
@@ -221,8 +226,12 @@ function ParametersSection({
     );
 }
 
-function ResultsView({compoundPeriod, results}: {
-    compoundPeriod: PERIOD,
+function ResultsStatisticsView({results} : {results: [[number], [number]]}) {
+    return <div></div>
+}
+
+function ResultsTableView({contributionPeriod, results}: {
+    contributionPeriod: PERIOD,
     results: [[number], [number]]
 }) {
     const [rows, setRows] = useState([{
@@ -238,7 +247,7 @@ function ResultsView({compoundPeriod, results}: {
             <Table sx={{minWidth: 650}} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>{periodToUnit(compoundPeriod)}</TableCell>
+                        <TableCell>{periodToUnit(contributionPeriod)}</TableCell>
                         <TableCell align="left">Deposits</TableCell>
                         <TableCell align="left">Interest</TableCell>
                         <TableCell align="left">Total Deposits</TableCell>
@@ -273,11 +282,21 @@ function App() {
     const [contribution, setContribution] = useState('0')
     const [contributionPeriod, setContributionPeriod] = useState(PERIOD.ANNUALLY)
     const [yearsCount, setYearsCount] = useState('0')
-    const [compoundPeriod, setCompoundPeriod] = useState(PERIOD.ANNUALLY)
     const [results, setResults] = useState([[0], [0]])
 
     function handleCalculate(e: any): void {
-
+        let principialInt = parseInt(principal)
+        let interestRateInt = parseInt(interestRate)
+        let contributionInt = parseInt(contribution)
+        let yearsInt = parseInt(yearsCount)
+        setResults(calculateResults(
+            principialInt,
+            interestRateInt,
+            interestRatePeriod,
+            contributionInt,
+            contributionPeriod,
+            yearsInt
+        ))
     }
 
     return <div className="App">
@@ -296,11 +315,12 @@ function App() {
                     setContributionPeriod={setContributionPeriod}
                     yearsCount={yearsCount}
                     setYearsCount={setYearsCount}
-                    compoundPeriod={compoundPeriod}
-                    setCompoundPeriod={setCompoundPeriod}
                     handleCalculate={handleCalculate}
                 />
-                <ResultsView compoundPeriod={compoundPeriod} results={results as [[number], [number]]}/>
+                <ResultsTableView
+                    contributionPeriod={contributionPeriod}
+                    results={results as [[number], [number]]}
+                />
             </Stack>
         </Container>
     </div>
